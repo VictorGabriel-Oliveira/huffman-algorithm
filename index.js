ce44dffa22c0f6678ad102541645536038f7a9ce
victor gabriel <deoliveiraabreuvictorgabriel@gmail.com>
2022-04-06 13:42:33 -0300

    create function for get binary tree

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 let simbolsFrequrnciArray = []
+class Node {  
+    constructor(value, char, left, right) {  
+        this.val  =value; // number of character occurrences  
+        this.char  =char; // character to be encoded  
+        this.left = left;  
+        this.right = right;  
+    }  
+}
 const getFrequenciSimbols = (text) => {
     for(let i = 0;i < text.length;i++){
         countFrenquenci(text.charAt(i))
@@ -18,8 +26,48 @@ const countFrenquenci = (simbol) => {
 }
 
 
+const getBinaryTree = (listOfChar) => {
+    let tree = [] 
+
+    for(let char in listOfChar){
+        let currentChar = listOfChar[char]
+
+        let node = new Node(currentChar.frequenci, currentChar.simbol)
+        tree.push(node)
+    }
+
+    let allNodes = []
+
+    while(tree.length !== 1){
+        tree.sort((a,b)=>{
+            
+            return a.val - b.val
+        })
+
+        let node = new Node(tree[0].val + tree[1].val, '') // setando a raiz da arvore 
+        
+        allNodes.push(tree[0])
+        allNodes.push(tree[1])
+
+        node.left = allNodes[ allNodes.length -2 ]
+        node.right = allNodes[ allNodes.length - 1 ]
+
+        tree = tree.slice(2)
+
+        tree.push(node)
+        
+
+
+    }
+
+    return tree[0]
+
+}
+
 
 module.exports = {
     getFrequenciSimbols,
-    countFrenquenci
+    countFrenquenci,
+    getBinaryTree
+    
 }
