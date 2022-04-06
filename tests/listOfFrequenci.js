605a23f95e02eff256c27ef5ecd02b2ed4182ba8
victor gabriel <deoliveiraabreuvictorgabriel@gmail.com>
2022-04-06 13:47:20 -0300

    create test of  function for get frequenci of char

---
+++ tests/listOfFrequenci.js
@@ -0,0 +1,18 @@
+const { getFrequenciSimbols } = require("../index")
+
+ test('return a list whith frequenci elements', ()=>{
+     const response = getFrequenciSimbols("hello world")
+    const responseExpeted = [
+        { simbol: 'h', frequenci: 1 },
+        { simbol: 'e', frequenci: 1 },
+        { simbol: 'l', frequenci: 3 },
+        { simbol: 'o', frequenci: 2 },
+        { simbol: ' ', frequenci: 1 },
+        { simbol: 'w', frequenci: 1 },
+        { simbol: 'r', frequenci: 1 },
+        { simbol: 'd', frequenci: 1 }
+      ]
+    expect(response).toEqual(responseExpeted)
+ })
+
+ 
